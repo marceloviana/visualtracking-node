84e9047b501c3cecb961c8da0a528db5f2e92e47
Marcelo Viana <marceloviana@macbook.local>
2025-01-17 11:16:43 -0300

    update github actions

--- .ansible/configure-ansible.sh
+++ .ansible/configure-ansible.sh
@@ -5,6 +5,7 @@ PUBLIC_IP="$1"
 USER="$2"
 PRIVATE_KEY_PATH="$3"
 run_ansible_for_ec2_instance=$4
+SERVER_PORT=$5
 timeout=3
 
 if [ "$run_ansible_for_ec2_instance" == "no"];then
@@ -16,7 +17,7 @@ while true;
 do
 
     sleep 3
-    ssh -o StrictHostKeychecking=no -o ConnectTimeout=5 -i $PRIVATE_KEY_PATH $USER@$PUBLIC_IP -p 9022 "echo `date`" > /dev/null 2>&1
+    ssh -o StrictHostKeychecking=no -o ConnectTimeout=5 -i $PRIVATE_KEY_PATH $USER@$PUBLIC_IP -p $SERVER_PORT "echo `date`" > /dev/null 2>&1
     if [ $? -eq 0 ];then
         echo "EC2 connect successfully"
         break
@@ -25,7 +26,7 @@ do
  
     ((timeout--))
     if [ $timeout -eq 0 ];then
-        echo "Impossível conectar ao EC2"
+        echo "Impossível conectar em $PUBLIC_IP:$SERVER_PORT"
         exit 1
     fi
 
@@ -34,12 +35,12 @@ done
 echo "Configurando Ansible /etc/ansible/hosts"
 cat <<EOF > /etc/ansible/hosts
 [aws_devops]
-$PUBLIC_IP ansible_ssh_private_key_file=$PRIVATE_KEY_PATH ansible_user=$USER
+$PUBLIC_IP:$SERVER_PORT ansible_ssh_private_key_file=$PRIVATE_KEY_PATH ansible_user=$USER
 
 [aws_devops:vars]
 ansible_python_interpreter=/usr/bin/python3
 EOF
 
 echo `pwd`
-echo "Implantando configurações em $PUBLIC_IP com Ansible..."
+echo "Implantando configurações em $PUBLIC_IP:$SERVER_PORT com Ansible..."
 cd ./ansible && /root/.local/bin/ansible-playbook -i /etc/ansible/hosts playbook.yml
