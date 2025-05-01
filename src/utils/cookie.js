be744564138cb7657864f5af2d454faa1304539e
Marcelo Viana <marceloviana@macbook.local>
2025-05-01 11:38:42 -0300

    fix: pipeline Thu May  1 11:38:42 -03 2025 - setcookie

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -3,23 +3,61 @@ const cookie = require('cookie')
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = process.env.JWT_SECRET
 
+const setCookieWithHTTPS = (res, user, token) => {
+  res.setHeader('Set-Cookie', [
+    `user_meta=${user}; HttpOnly; Secure; Path=/; Max-Age=86400`,
+    `auth_token=${token}; HttpOnly; Secure; Path=/; Max-Age=86400`
+  ]);  
+}
+
+const setCookieWithoutHTTPS = (res, user, token) => {
+  res.setHeader('Set-Cookie', [
+    `user_meta=${user}; Path=/; Max-Age=86400`,
+    `auth_token=${token}; Path=/; Max-Age=86400`
+  ]);  
+}
+
+const deleteCookieWithHTTPS = (res, user, token) => {
+  res.setHeader('Set-Cookie', [
+    `user_meta=${user}; HttpOnly; Secure; Path=/; Max-Age=0`,
+    `auth_token=${token}; HttpOnly; Secure; Path=/; Max-Age=0`
+  ]);  
+}
+
+const deleteCookieWithoutHTTPS = (res) => {
+  res.setHeader('Set-Cookie', [
+    `user_meta=${user}; Path=/; Max-Age=0`,
+    `auth_token=${token}; Path=/; Max-Age=0`
+  ]);
+}
+
 const setCookie = async (req, res, user) => {
 
     const token = jwt.sign({user}, JWT_SECRET, { expiresIn: '1d' })
   
-    res.setHeader('Set-Cookie', [
-      `user_meta=${user}; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=86400`,
-      `auth_token=${token}; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=86400`
-    ]);
+    // res.setHeader('Set-Cookie', [
+    //   `user_meta=${user}; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=86400`,
+    //   `auth_token=${token}; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=86400`
+    // ]);
+    if (req.protocol == 'https') {
+      setCookieWithHTTPS(res, user, token)
+    } else {
+      setCookieWithoutHTTPS(res, user, token)
+    }
 
 }
 
 const deleteCookie = async (req, res) => {
 
-  res.setHeader('Set-Cookie', [
-    `user_meta=; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=0`,
-    `auth_token=; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=0`
-  ]);
+  // res.setHeader('Set-Cookie', [
+  //   `user_meta=; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=0`,
+  //   `auth_token=; ${req.protocol == 'https'? 'HttpOnly': ''}; Path=/; Max-Age=0`
+  // ]);
+  if (req.protocol == 'https') {
+    deleteCookieWithHTTPS(res)
+  } else {
+    deleteCookieWithoutHTTPS(res)
+  }  
 
 }
 
