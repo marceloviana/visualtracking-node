58f503ff42f10dcafff0378aca8d171a51177069
Marcelo Viana <marceloviana@macbook.local>
2025-05-02 07:12:26 -0300

    fix: cookie Fri May  2 07:12:26 -03 2025

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -6,20 +6,23 @@ const JWT_SECRET = process.env.JWT_SECRET
 const setCookie = async (req, res, user) => {
 
     const token = jwt.sign({user}, JWT_SECRET, { expiresIn: '1d' })
-    console.log('host:::', req.headers)
+    let origin = req.headers.origin || '.infsite.org'
+    let domain = origin.replace(/.*:\/\//gi, '')
+    console.log('origin:::', domain)
+
     await res.cookie("auth_token", token, {
       "httpOnly": true,
       "secure": req.protocol == 'https'? true : false,
       "sameSite": "Lax",
       "maxAge": 60 * 60 * 1000,
-      "domain": req.headers.origin.replace(`${req.protocol}://`, '')
+      "domain": domain
     });
     await res.cookie("user_meta", user, {
       "httpOnly": false,
       "secure": req.protocol == 'https'? true : false,
       "sameSite": "Lax",
       "maxAge": 60 * 60 * 1000,
-      "domain": req.headers.origin.replace(`${req.protocol}://`, '')
+      "domain": domain
     });
 
 }
